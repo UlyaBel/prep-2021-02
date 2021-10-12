bcd20ac0ef453438b440caae5cc2dec0b976a50f
Ulya_Belyanina <ulya_bel11@mail.ru>
2021-10-13 00:49:37 +0300

    First commit in DZ_1

--- project/src/main.c
+++ project/src/main.c
@@ -1,12 +1,13 @@
 #include "utils.h"
+#include "string.h" //--- size_t work with yhis library
+#include "stddev.h" // ---
 
 #define ERR_ARGS_COUNT (-1)
 #define ERR_WRONG_FLG (-2)
 
-#define TST_FOO_FIX     1
-#define TST_FOO_IMPL    2
-#define TST_MOD_IMPL    3
-
+#define TST_FOO_FIX 1
+#define TST_FOO_IMPL 2
+#define TST_MOD_IMPL 3
 
 /* NOTE(stitaevskiy):
  * We use `atoi` function just for simplification and code reducing.
@@ -23,42 +24,52 @@
  *
  * */
 
-int main(int argc, const char** argv) {
-    if (argc < 3) {
+int main(int argc, const char **argv)
+{
+    if (argc < 3)
+    {
         return ERR_ARGS_COUNT;
     }
 
     int Test_case = atoi(argv[1]);
-    const char* data;
+    const char *data;
     data = argv[2];
 
-    switch (Test_case) {
-        case TST_FOO_FIX: {
-            int to = atoi(data);
-            size_t ticks_count = timer_from(to);
-            printf("%d\n", ticks_count);
-            break;
-        }
-        case TST_FOO_IMPL: {
-            if (argc = 4) {
-                // int base = atoi(data);
-                // int pow =  atoi(argv[3]);
-                // int res = custom_pow(base, pow);    // TODO: Implement me
+    switch (Test_case)
+    {
+    case TST_FOO_FIX:
+    {
+        int to = atoi(data);
+        size_t ticks_count = timer_from(to);
+        printf("%zu\n", ticks_count); // --- because size_t
+        break;
+    }
+    case TST_FOO_IMPL:
+    {
+        if (argc == 4) // --- must be ==
+        {
+            // int base = atoi(data);
+            // int pow =  atoi(argv[3]);
+            // int res = custom_pow(base, pow);    // TODO: Implement me
 
-                // printf("%i\n", res);
-            } else {
-                return ERR_ARGS_COUNT;
-            }
+            // printf("%i\n", res);
         }
-        case TST_MOD_IMPL: {
-            // int num = atoi(data);
+        else
+        {
+            return ERR_ARGS_COUNT;
+        }
+    }
+    case TST_MOD_IMPL:
+    {
+        // int num = atoi(data);
 
-            // TODO: Print to stdout `1` if `num` is prime number and `0` otherwise
-            // This function MUST be implemented in
-            // a separate C-module (not in `main` or `utils` module)
-        }
-        default: {
-            return ERR_WRONG_FLG;
-        }
+        // TODO: Print to stdout `1` if `num` is prime number and `0` otherwise
+        // This function MUST be implemented in
+        // a separate C-module (not in `main` or `utils` module)
+    }
+    default:
+    {
+        return ERR_WRONG_FLG;
+    }
     }
 }
--- project/src/utils.c
+++ project/src/utils.c
@@ -1,10 +1,13 @@
 #include "utils.h"
+#include "string.h" // --- size_t work with yhis library
 
-size_t timer_from(unsigned char from) {
-    size_t counter;
-    for (unsigned char i = from; i >= 0; --i) {
+size_t timer_from(unsigned char from)
+{
+    size_t counter = 0;              // --- counter need initial value
+    for (char i = from; i >= 0; i--) // --- i must be signed char
+    {
         ++counter;
-        printf(i);
+        printf("%d", i); // --- wrong out without %d
     }
     return counter;
 }
