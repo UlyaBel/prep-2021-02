d83ae7be004b8b23bf7f9554b3cb47a430b28851
Ulya_Belyanina <ulya_bel11@mail.ru>
2021-10-15 02:03:39 +0300

    Test commit after rename 2

--- project/src/utils.c
+++ project/src/utils.c
@@ -1,12 +1,14 @@
-#include <stdio.h>  // ---
+#include <stdio.h> // ---
 #include "utils.h"
-#include <string.h>  // --- size_t work with yhis library
+#include <string.h> // --- size_t work with this library
 
-size_t timer_from(unsigned char from) {
-    size_t counter = 0;  // --- counter need initial value
-    for (char i = from; i >= 0; i--) {  // --- i must be signed char
+size_t timer_from(unsigned char from)
+{
+    size_t counter = 0; // --- counter need initial value
+    for (char i = from; i >= 0; i--)
+    { // --- i must be signed char
         ++counter;
-        printf("%d", i);  // --- wrong out without %d
+        printf("%d", i); // --- wrong out without %d
     }
     return counter;
 }
